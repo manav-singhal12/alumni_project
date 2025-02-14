3c319add38d62472f970f85345041231fb37cca2
manav-singhal12 <manavsinghal165@gmail.com>
2025-02-15 01:56:50 +0530

    d12

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server:{
     proxy:{
-      "/api":"https://alumni-project-3.onrender.com/"
+      "/api":"https://alumni-project-3.onrender.com/",
+      "/socket.io":"https://alumni-project-3.onrender.com/",
+      "/server":" https://alumni-project-qwg6.vercel.app",
     }
   }
 })
