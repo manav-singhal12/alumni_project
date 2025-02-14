6940aa2fe7c45e604ab743893abf3dadc8c9090b
Naveen <naveenyadav11k@gmail.com>
2025-02-15 04:44:31 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{
-    proxy:{
-      "/api":"https://alumni-project-i1qf.onrender.com"
-    }
-  }
-})
+  plugins: [react(), tailwindcss()],
+  // server: {
+  //   proxy: {
+  //     "/api/v1/": "https://alumni-kid1.onrender.com/",
+  //     "/socket.io": {
+  //       target: "https://alumni-project-3.onrender.com",
+  //       ws: true,
+  //     },
+  //   },
+  // },
+});
