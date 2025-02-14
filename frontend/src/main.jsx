29cece5bc7ae1a420273959f65e44776402aba70
manav-singhal12 <manavsinghal165@gmail.com>
2025-02-15 01:35:33 +0530

    d5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { createBrowserRouter, createRoutesFromElements, Outlet } from 'react-router';
+import { createBrowserRouter, createRoutesFromElements, Outlet } from 'react-router-dom';
 import { Route, Navigate, RouterProvider } from 'react-router-dom';
 import { Provider, useSelector } from 'react-redux';
 import store from './redux/store.js';
